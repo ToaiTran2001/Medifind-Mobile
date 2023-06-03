9c2c8af7e20106ad46a41ca587b8d82004e0a808
baokhanhle123 <baokhanhle27082001@gmail.com>
2023-06-03 14:43:58 +0700

    Implement MediGPT

---
+++ env.js
@@ -0,0 +1,13 @@
+export const dialogflowConfig = {
+    "type": "service_account",
+    "project_id": "medigpt-yrng",
+    "private_key_id": "4ab439c602edc8410dbcf8a6dff6aafb7a138d30",
+    "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQDUfyrm1O4J7baC\npOToWKca5yhbidb7mjJTITEKeI2+chPTSSyEPUSMnsp0GqZvoB7HqlOMgNV3wpnK\nxwi+Sykl7ZgEXH7ES74wa4JAKHzKRoMVJ7MqkUa4fjDxtpV0ArtUoHP/Zcod81tR\nHCEF69b5Ux5tdfKLkxYVC16kst5/JVbw7Os8yDi7RLnMKTxBSOO09/CiN+zVlL/D\nnoc44Bm6wEVr2jWWtTET3khJbmO6cNX7TRD+nFgfWKTgmeuTfS1rGNMQThD1Cx97\nwyf/Qn7Lif0Xr7rl2Gbz0E4sEdm9OEo5AeyhgwN0Ei8sR64V1qf0Sd2iO9upWdMp\nPgmOBE71AgMBAAECggEAHP+Tq7eQGnPBrddx05FRkJbOB5vqZiB3UKfdaOfltAEe\nWn2IYS15PtVmnGyMnPwVZzsDts9YEkVQtn7t3wYjweTIE4RJ2mDZvgLV3gqIaRQx\nTEBEL4FuzLuP01SO1auPw+83lTlfWTH+GrYDfarfBdxFcTug01JJt6p7Cm/MdD41\nEqyDwPARDMwdyPIsgWoFPOsOiC6S5tb/e4n1Ou/luKlQf9vaJmq11tpv+OhmnaLY\nhWlu4GmYGgcdMercMqGSRCdQxEXVODgv5j4gZLNdduwYZ0+e3Deeg8IWpKGUvUKk\nca9kkCrQKH/lH/TcBUmAi2qHc3vMNxb8sE8YNFTN0QKBgQDsZnmJi6IjttEGsL3G\nZtiV9TJUC/7uI8Ttiog+rrur/geMNPs/o1eKyUhzx6bpc106Jde0Hos1Uiz/dgCZ\n0i1OaQucGSZ1pmk2d9BPC7/PNIKIPlg2WhP30WmxHwDxksJmn9UZmn1siaRkWKnZ\nTFAu6SREdhUy9fqj0vhB1sedjQKBgQDmHVjoBsTJ+6yEJb1tTnOa/jxU62BdcWVj\nLLHKZWKf15HrOuWZ7j8ABrWi8tzNTQgRlg6qWdW54/akZjttyu+ya2LYmnswevlT\nLzLu2odN0OQf+ykc9bhcxodZF06cJQokVjatoNZSXcSxDgnx+slqSkZ6GcC8rvyN\nbi4QjVEZCQKBgA5VsGz8wJo1zbsAIKHVPrDpK2IZUB4UoS5eRiC2675kKwT/eXTE\nbNyHlBMhOifRzQdnoArWB072arAa8bJiIkks6X1PsRtVOfAJl+ITLlBcOMYWMbcq\nAyf11Njermn3oj4i1wM0cQKR3hwPnE0G6Z3OmVplZqeubsJcWp1zbqtJAoGBAOK4\nI3JnyNegqTaB4IKjeu9IcbTRO16VbOkrjff1sBgAUnpKyxDAVT33XlovojB8Juj8\nyXI5lIPUAJu1RfREr6lAeGEkwx7e6mt041/F2u1mVHzV/onT51MxHwU+G2toavem\nIbUr9D2uuS4gwNey2WNpzspaOTUtyxKlbalNMovRAoGBAJQHthFV8jB4yOm9Zdn9\nudzLUSkj4FyJ9/0I6a+kPUoetkeWErpKSzoLddSJD+ajKIkwVEmXJn4TOYL1VAKC\nrco+reUGky4aEwMqhHmRiBVMhT6vzUBMJkGa293I51F1RVTrV7STZ9gF8h4YfCnU\nZxNq9sPvNVtcpjP1XQ6MErPS\n-----END PRIVATE KEY-----\n",
+    "client_email": "medigpt@medigpt-yrng.iam.gserviceaccount.com",
+    "client_id": "111027732488234403455",
+    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+    "token_uri": "https://oauth2.googleapis.com/token",
+    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+    "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/medigpt%40medigpt-yrng.iam.gserviceaccount.com",
+    "universe_domain": "googleapis.com"
+}
